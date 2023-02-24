4b476f617abde5feb5b3869cbb8039935cd0f2a7
Balajiw920 <wbalaji2021@gmail.com>
2023-02-24 20:37:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here
 
+// get elements from the DOM
+const newTodoInput = document.getElementById("newTodoInput");
+const addTodoBtn = document.getElementById("addTodoBtn");
+const todoList = document.getElementById("todoList");
+
+// add event listener to the Add todo button
+addTodoBtn.addEventListener("click", () => {
+  const newTodo = newTodoInput.value.trim(); // trim to remove leading/trailing whitespaces
+  
+  // validate the input
+  if (newTodo === "") {
+    alert("Please enter a todo item."); // show an alert if the input is empty
+    return;
+  }
+  
+  // create a new list item element and append it to the list
+  const listItem = document.createElement("li");
+  listItem.textContent = newTodo;
+  todoList.appendChild(listItem);
+  
+  // clear the input field
+  newTodoInput.value = "";
+});
